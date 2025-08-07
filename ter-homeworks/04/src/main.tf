d333a23de413c8fd7412ab97636f9231ae9ff953
Dmitrii Osipov <dimosspb@vk.com>
2025-08-07 13:06:53 +0300

    terr-04-task6 start

--- ter-homeworks/04/src/main.tf
+++ ter-homeworks/04/src/main.tf
@@ -1,76 +1,17 @@
-# resource "yandex_vpc_network" "develop" {
-#   name = var.vpc_name
-# }
-# resource "yandex_vpc_subnet" "develop" {
-#   name           = var.vpc_name
-#   zone           = var.default_zone
-#   network_id     = yandex_vpc_network.develop.id
-#   v4_cidr_blocks = var.default_cidr
-# }
-
-
-module "vpc_prod" {
-  source = "./modules/vpc"
-  name     = "production"
-  subnets = [
-    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
-    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
-    { zone = "ru-central1-d", cidr = "10.0.3.0/24" },   # ru-central1-c - НЕТ!
-  ]
+# To always have a unique bucket name in this example
+resource "random_string" "unique_id" {
+  length  = 8
+  upper   = false
+  lower   = true
+  numeric = true
+  special = false
 }
 
-module "vpc_dev" {
-  source = "./modules/vpc"
-  name     = "develop"
-  subnets = [
-    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
-  ]
-}
+module "s3" {
+  source = "https://github.com/terraform-yc-modules/terraform-yc-s3/tree/master"
 
-module "vm-marketing" {
-  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "prod"
-  network_id     = module.vpc_prod.vpc_id
-  subnet_zones   = [module.vpc_prod.subnet_zones[0]]
-  subnet_ids     = [module.vpc_prod.subnet_ids[0]]
-  instance_name  = var.projects_data.projects[0]
-  instance_count = 1
-  image_family   = data.yandex_compute_image.ubuntu.family
-  public_ip      = true
-
-  metadata =  {
-    serial-port-enable = 1,
-    user-data = data.template_file.metadata.rendered
+  bucket_name = "simple-bucket-${random_string.unique_id.result}"
+  versioning = {
+    enabled = true
   }
-
-
-  labels = {
-    project     = var.projects_data.projects[0]
-  }
-
-  depends_on = [ module.vpc_prod ]
-}
-
-module "vm-analytics" {
-  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "prod"
-  network_id     = module.vpc_prod.vpc_id
-  subnet_zones   = [module.vpc_prod.subnet_zones[0]]
-  subnet_ids     = [module.vpc_prod.subnet_ids[0]]
-  instance_name  = var.projects_data.projects[1]
-  instance_count = 1
-  image_family   = data.yandex_compute_image.ubuntu.family
-  public_ip      = true
-
-  metadata =  {
-    serial-port-enable = 1,
-    user-data = data.template_file.metadata.rendered
-  }
-
-
-  labels = {
-    project     = var.projects_data.projects[1]
-  }
-
-  depends_on = [ module.vpc_prod ]
 }
